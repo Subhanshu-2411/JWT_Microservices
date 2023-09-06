1f6ad69edd330d226008eb30388365b66b29aad7
Subhanshu Bansal <subhanshu20135@iiitd.ac.in>
2023-09-06 08:32:48 +0530

    Initial Commit

---
+++ src/test/java/com/example/jwt_microservices/JwtMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwt_microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtMicroservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

4eec8d1a9c30de7f026a764f7e2a85bbb970957a
Subhanshu Bansal <subhanshu20135@iiitd.ac.in>
2023-09-07 22:33:35 +0530

    GET Request added to Controller

--- src/main/java/com/example/jwt_microservices/controller/HomeController.java
+++ src/main/java/com/example/jwt_microservices/controller/HomeController.java
@@ -1,16 +1,24 @@
 package com.example.jwt_microservices.controller;
 
+import com.example.jwt_microservices.model.Home;
+import com.example.jwt_microservices.service.HomeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/home")
 public class HomeController {
 
-    @GetMapping("/user")
-    public String getUser() {
-        System.out.println("Getting Users");
-        return "Users";
+    @Autowired
+    private HomeService homeService;
+
+    @GetMapping("/home")
+    public List<Home> getHomes() {
+        System.out.println("Getting Homes");
+        return homeService.getHomes();
     }
 }

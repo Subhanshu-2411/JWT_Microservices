8c65fefb9871795f0e0cd6d4c0f3adce3c8eb8c4
Subhanshu Bansal <subhanshu20135@iiitd.ac.in>
2023-09-07 22:31:04 +0530

    Added Home Model & a Service Object GET Request

--- src/main/java/com/example/jwt_microservices/service/HomeService.java
+++ src/main/java/com/example/jwt_microservices/service/HomeService.java
@@ -1,4 +1,23 @@
 package com.example.jwt_microservices.service;
 
+import com.example.jwt_microservices.model.Home;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
 public class HomeService {
+    private final List<Home> homes = new ArrayList<>();
+
+    public HomeService() {
+        homes.add(new Home(UUID.randomUUID().toString(), "Subhanshu"));
+        homes.add(new Home(UUID.randomUUID().toString(), "Manu"));
+        homes.add(new Home(UUID.randomUUID().toString(), "Ananyaa"));
+        homes.add(new Home(UUID.randomUUID().toString(), "Tanu"));
+        homes.add(new Home(UUID.randomUUID().toString(), "Hello"));
+    }
+
+    public List<Home> getHomes() {
+        return this.homes;
+    }
 }
